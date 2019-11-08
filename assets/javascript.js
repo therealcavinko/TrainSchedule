dd463fddb8e75dc333c862b98d5c0e4b58136ad0
TheRealCalvinKO <calvinknockout@gmail.com>
2019-11-08 18:13:50 -0500

    js

--- assets/javascript.js
+++ assets/javascript.js
@@ -4,22 +4,20 @@ $(document).ready(function(){
         databaseURL: "https://train-scheduler-68643.firebaseio.com",
     };
     firebase.initializeApp(config);
+//-------------------------------------------------------------------------------------------------------------
 
     var database = firebase.database();
     var name;
     var destination;
     var firstTrain;
     var frequency = 0;
+//-------------------------------------------------------------------------------------------------------------
 
     $("#add-train").on("click", function() {
-        event.preventDefault();
-        
         name = $("#train-name").val().trim();
         destination = $("#destination").val().trim();
         firstTrain = $("#first-train").val().trim();
         frequency = $("#frequency").val().trim();
-
-        
         database.ref().push({
             name: name,
             destination: destination,
@@ -29,9 +27,9 @@ $(document).ready(function(){
         });
         $("form")[0].reset();
     });
+//-------------------------------------------------------------------------------------------------------------
 
     database.ref().on("child_added", function(childSnapshot) {
-        var nextArr;
         var minAway;
         
         var firstTrainNew = moment(childSnapshot.val().firstTrain, "hour/min").subtract(1);
@@ -49,16 +47,7 @@ $(document).ready(function(){
                 "</td><td>" + childSnapshot.val().frequency +
                 "</td><td>" + nextTrain + 
                 "</td><td>" + minAway + "</td></tr>");
-
+//-------------------------------------------------------------------------------------------------------------
             
         });
-    
-
-    database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
-        
-        $("#name-display").html(snapshot.val().name);
-        $("#email-display").html(snapshot.val().email);
-        $("#age-display").html(snapshot.val().age);
-        $("#comment-display").html(snapshot.val().comment);
-    });
 });
